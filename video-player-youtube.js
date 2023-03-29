e889a5f8c811d7148145361d855c4af4069646f6
Bodacious <94692022+bdcsstudio@users.noreply.github.com>
2023-03-29 09:46:09 +0200

    Create video-player-youtube.js

---
+++ video-player-youtube.js
@@ -0,0 +1,54 @@
+$(document).ready(function () {
+  // Check if the script is being executed within a div with the attribute of '[youtube-player="true"]'
+  var youtubePlayer = $('[youtube-player="true"]');
+  if (youtubePlayer.length) {
+    youtubePlayer.find('[youtube-embed="true"]').each(function () {
+      var video = $(this).children("iframe").get(0);
+      var src = video.src;
+
+      // Handle open and close events
+      $(this)
+        .closest("[youtube-player='true']")
+        .find('[youtube-open="true"]')
+        .click(function (e) {
+          e.preventDefault();
+          var autoplaySrc = src + "?autoplay=1";
+          video.src = autoplaySrc;
+        });
+      $(this)
+        .closest("[youtube-player='true']")
+        .find('[youtube-close="true"]')
+        .click(function (e) {
+          e.preventDefault();
+          video.src = "";
+        });
+
+      // Handle play, pause, and toggle events
+      $(this)
+        .closest("[youtube-player='true']")
+        .find('[youtube-play="true"]')
+        .click(function (e) {
+          e.preventDefault();
+          video.play();
+        });
+      $(this)
+        .closest("[youtube-player='true']")
+        .find('[youtube-pause="true"]')
+        .click(function (e) {
+          e.preventDefault();
+          video.pause();
+        });
+      $(this)
+        .closest("[youtube-player='true']")
+        .find('[youtube-toggle="true"]')
+        .click(function (e) {
+          e.preventDefault();
+          if (video.paused) {
+            video.play();
+          } else {
+            video.pause();
+          }
+        });
+    });
+  }
+});
