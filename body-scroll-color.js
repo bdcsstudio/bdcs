5049bf79c09d639a89c44279b1b55605d049b89a
Bodacious <94692022+bdcsstudio@users.noreply.github.com>
2023-03-29 09:36:49 +0200

    Create body-scroll-color.js

---
+++ body-scroll-color.js
@@ -0,0 +1,37 @@
+// PAGE COLOR POWER-UP
+window.addEventListener("DOMContentLoaded", (event) => {
+  // attribute value checker
+  function attr(defaultVal, attrVal) {
+    const defaultValType = typeof defaultVal;
+    if (typeof attrVal !== "string" || attrVal.trim() === "") return defaultVal;
+    if (attrVal === "true" && defaultValType === "boolean") return true;
+    if (attrVal === "false" && defaultValType === "boolean") return false;
+    if (isNaN(attrVal) && defaultValType === "string") return attrVal;
+    if (!isNaN(attrVal) && defaultValType === "number") return +attrVal;
+    return defaultVal;
+  }
+  // pagecolor trigger
+  $("[body-color='trigger']").each(function (index) {
+    // elements
+    let triggerEl = $(this),
+      targetEl = $("body");
+    // settings
+    let classSetting = attr(
+      "body-color-light",
+      triggerEl.attr("body-color-class")
+    );
+    // result
+    ScrollTrigger.create({
+      trigger: triggerEl,
+      start: "top center",
+      end: "bottom center",
+      onToggle: ({ self, isActive }) => {
+        if (isActive) {
+          targetEl.addClass(classSetting);
+        } else {
+          targetEl.removeClass(classSetting);
+        }
+      }
+    });
+  });
+});
