bcd909d389828c1cac4c232d60860e83774c6496
Bodacious <94692022+bdcsstudio@users.noreply.github.com>
2023-03-29 09:45:35 +0200

    Create video-player-vimeo.js

---
+++ video-player-vimeo.js
@@ -0,0 +1,51 @@
+$("[vimeo-player='true'] [vimeo-embed='true']").each(function () {
+  var player = new Vimeo.Player(this);
+
+  // Open lightbox with autoplay
+  $(this)
+    .closest("[vimeo-player='true']")
+    .find("[vimeo-open=true]")
+    .click(function () {
+      player.setCurrentTime(0);
+      player.setMuted(false);
+      player.play();
+    });
+
+  // Close lightbox when open modal is set
+  $(this)
+    .closest("[vimeo-player='true']")
+    .find("[vimeo-close=true]")
+    .click(function () {
+      player.setMuted(true);
+      player.pause();
+    });
+
+  // Play button within lightbox
+  $(this)
+    .closest("[vimeo-player='true']")
+    .find("[vimeo-play=true]")
+    .click(function () {
+      player.play();
+    });
+
+  // Pause button within lightbox
+  $(this)
+    .closest("[vimeo-player='true']")
+    .find("[vimeo-pause=true]")
+    .click(function () {
+      player.pause();
+    });
+
+  $(this)
+    .closest("[vimeo-player='true']")
+    .find("[vimeo-toggle=true]")
+    .click(function () {
+      player.getPaused().then(function (paused) {
+        if (paused) {
+          player.play();
+        } else {
+          player.pause();
+        }
+      });
+    });
+});
