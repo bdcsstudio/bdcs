dee749a2c7d8723e1f545e03b9bca100aac9bad0
Bodacious <94692022+bdcsstudio@users.noreply.github.com>
2023-04-06 15:24:47 +0200

    Update nav-hover-spotlight.js

--- nav-hover-spotlight.js
+++ nav-hover-spotlight.js
@@ -1,1 +1,22 @@
 
+  document.addEventListener("DOMContentLoaded", function() {
+    const navSpotlightItems = document.querySelectorAll('[nav-spotlight-list="true"] [nav-spotlight-item="true"]');
+
+    navSpotlightItems.forEach(item => {
+      item.addEventListener("mouseenter", function() {
+        const siblings = item.parentNode.querySelectorAll('[nav-spotlight-item="true"]');
+        siblings.forEach(sibling => {
+          if (sibling !== item) {
+            sibling.style.opacity = "0.5";
+          }
+        });
+      });
+
+      item.addEventListener("mouseleave", function() {
+        const siblings = item.parentNode.querySelectorAll('[nav-spotlight-item="true"]');
+        siblings.forEach(sibling => {
+          sibling.style.opacity = "1";
+        });
+      });
+    });
+  });
