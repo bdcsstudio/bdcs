1777648299c7548366c6c3c397266115c4263755
Bodacious <94692022+bdcsstudio@users.noreply.github.com>
2023-04-24 12:17:54 +0200

    Update page-transition.js

--- page-transition.js
+++ page-transition.js
@@ -1,24 +1,27 @@
 // Works on internal links, ignores external links or links containing #
 function internalLink(myLink) {
-  return myLink.host == window.location.host;
+    return (myLink.host == window.location.host);
 }
-$("a").each(function () {
-  if (internalLink(this) && this.href.indexOf("#") === -1) {
-    $(this).click(function (e) {
-      e.preventDefault();
-      var moduleURL = jQuery(this).attr("href");
-      var transitionTime = $(this).attr("page-transition-time") || 1000;
-      setTimeout(function () {
-        window.location = moduleURL;
-      }, transitionTime);
-      // Class that has page out interaction tied to click
-      $('[page-transition="component"]').click();
+$('a').each(function() {
+  if (internalLink(this) && (this).href.indexOf('#') === -1) {
+  $(this).click(function(e){
+  e.preventDefault();
+  var moduleURL = jQuery(this).attr("href");
+  var transitionTime = $(this).attr("page-transition-time") || 1000;
+  setTimeout( function() { window.location = moduleURL }, transitionTime );
+  
+  // Class that has page out interaction tied to click
+  $('[page-transition="component"]').click();
 
-      $(window).bind("pageshow", function (event) {
-        if (event.originalEvent.persisted) {
-          window.location.reload();
-        }
-      });
-    });
-  }
+  // Trigger custom event for GSAP interaction
+  $(document).trigger('page-transition');
+
+  $(window).bind("pageshow", function(event) {
+    if (event.originalEvent.persisted) {
+        window.location.reload() 
+    }
+  });
+
+  });
+ }
 });
